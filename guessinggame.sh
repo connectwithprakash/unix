08c86deec5dd810ea999dc868b0aedefa7131f04
Prakash Chaudhary <connectwithprakash@yahoo.com>
2020-05-11 19:58:20 +0545

    Add files via upload

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,24 +2,31 @@
 # File: guessinggame.sh
 
 function guess {
-	actual=$(ls -l | grep '^-' | wc -l)
+	actual=$(ls -al | grep '^-' | wc -l)
 	local found_ans=false
 	while [[ $found_ans = 'false' ]]
 	do
 		echo 'Enter your guess:'
 		read prediction
+
+		while ! [[ $prediction =~ ^[0-9]+$ ]]
+		do
+			echo 'You didnot enter number correctly, Try Again.'
+			read prediction
+		done
+
 		if [[ $prediction -lt $actual ]]
 		then
-			echo 'Your guess is less than what is true'
+			echo 'Your guess is less than what is true, Try Again.'
 		elif [[ $prediction -gt $actual ]]
 		then
-			echo 'Your guess is more than what is true'
+			echo 'Your guess is more than what is true, Try Again.'
 		else
-			echo 'You have correctly predicted the number of files in your current directory'
+			echo -e 'Congratulations!!!\nYou have correctly predicted the number of files in your current directory'
 			found_ans=true
 		fi
 	done
 }
 
-echo 'Guess the number of files in your current directory'
+echo 'How many files are there in your current directory?'
 guess 
